3af575153cd7f1e930f481b1875dfe7d7d8166a5
Khalid176 <muhammad.khalid2006@gmail.com>
2025-11-21 14:47:34 +0500

    working on constants

--- PF Project Skeleton/core/simulation_state.h
+++ PF Project Skeleton/core/simulation_state.h
@@ -13,18 +13,41 @@
 int const MAX_columns = 100 ;
 int const MAX_rows = 100 ;
 
+/* using extern int cause the row and column constants would be decalred in .cpp file and woudl be share here */
 
+extern int g_rows;
+extern int g_columns;
 
+extern char grid[MAX_columns][MAX_rows]; // THE MAXIMUM MAP
 
 // ----------------------------------------------------------------------------
 // TRAIN CONSTANTS
 // ----------------------------------------------------------------------------
+const int Trains_max = 100;
 const int TrainID = 0;   // using this to show the id now as it is not in the usual train code i would have to start from 1 when i am taking the data form the level file
 const int TrainTICK = 1; // first value in the given data
 const int T_x = 2;         // second value in the given data
 const int T_y = 3;         // thired value in the given data
 const int T_direction = 4; // fourth value in the given data
 const int T_colour = 5;    // fifth value in the given data
+
+// now taking destenation points to know where train needs to go so we can the use manhatan formula to evaluate the preority system
+const int T_des_X = 6; // sixth value int would be caluculated 
+const int T_des_Y = 7;
+
+// TO check which trinas are active and which are not 
+
+const int T_status = 8;
+
+// Taking an other constant for the total properties so that i can make an array of total train x total properties
+
+const int T_properties= 9;
+
+// array 
+extern int trains_data [Trains_max][T_properties];
+
+// For number of trains 
+extern int Number_Of_Trains;
 
 // ----------------------------------------------------------------------------
 // SWITCH CONSTANTS
