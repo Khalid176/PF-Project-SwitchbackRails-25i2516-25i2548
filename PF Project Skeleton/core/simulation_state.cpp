b20931dac818894d3de19b0c15fbc3fe62fe5daf
Khalid176 <muhammad.khalid2006@gmail.com>
2025-11-23 15:53:12 +0500

    done with variables and constant declaration also  made initializeSimulationState() to reset data for next level loading

--- PF Project Skeleton/core/simulation_state.cpp
+++ PF Project Skeleton/core/simulation_state.cpp
@@ -8,16 +8,22 @@
 // ----------------------------------------------------------------------------
 // GRID
 // ----------------------------------------------------------------------------
-
+int grid_rows = 0;
+int grid_columns = 0;
+char grid[MAX_rows][MAX_columns];
 
 // ----------------------------------------------------------------------------
 // TRAINS
 // ----------------------------------------------------------------------------
 
+int Number_Of_Trains = 0;
+int trains_data[Trains_max][T_properties];
 
 // ----------------------------------------------------------------------------
 // SWITCHES
 // ----------------------------------------------------------------------------
+
+int switch_data[MAX_switches][S_properties];
 
 // ----------------------------------------------------------------------------
 // SPAWN AND DESTINATION POINTS
@@ -26,14 +32,20 @@
 // ----------------------------------------------------------------------------
 // SIMULATION PARAMETERS
 // ----------------------------------------------------------------------------
-
+int tick = 0;
+int weather = 0;
+unsigned int seed = 0;
 // ----------------------------------------------------------------------------
 // METRICS
 // ----------------------------------------------------------------------------
-
+int trains_crashed;
+int trains_arrived;
+int total_switch_flips;
 // ----------------------------------------------------------------------------
 // EMERGENCY HALT
 // ----------------------------------------------------------------------------
+
+int emergency_halt_timer;
 
 // ============================================================================
 // INITIALIZE SIMULATION STATE
@@ -43,5 +55,38 @@
 // ----------------------------------------------------------------------------
 // Called before loading a new level.
 // ----------------------------------------------------------------------------
-void initializeSimulationState() {
+void initializeSimulationState()
+{
+    grid_columns = 0;
+    grid_rows = 0;
+    Number_Of_Trains = 0;
+    tick = 0;
+    weather = 0;
+    seed = 0;
+    trains_arrived = 0;
+    trains_crashed = 0;
+    total_switch_flips = 0;
+    emergency_halt_timer = 0;
+
+    for (int i = 0; i < MAX_rows; i++)
+    {
+        for (int j = 0; j < MAX_columns; j++)
+        {
+            grid[i][j] = ' ';
+        }
+    }
+    for (int i = 0; i < Trains_max; i++)
+    {
+        for (int j = 0; j < T_properties; j++)
+        {
+            trains_data[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < MAX_switches; i++)
+    {
+        for (int j = 0; j < S_properties ; j++)
+        {
+            switch_data[i][j] = 0;
+        }
+    }
 }
--- PF Project Skeleton/core/simulation_state.h
+++ PF Project Skeleton/core/simulation_state.h
@@ -10,44 +10,29 @@
 // ----------------------------------------------------------------------------
 // GRID CONSTANTS
 // ----------------------------------------------------------------------------
-int const MAX_columns = 100 ;
-int const MAX_rows = 100 ;
-
-/* using extern int cause the row and column constants would be decalred in .cpp file and woudl be share here */
-
-extern int g_rows;
-extern int g_columns;
-
-extern char grid[MAX_columns][MAX_rows]; // THE MAXIMUM MAP
+int const MAX_columns = 100;
+int const MAX_rows = 100;
 
 // ----------------------------------------------------------------------------
 // TRAIN CONSTANTS
 // ----------------------------------------------------------------------------
 const int Trains_max = 100;
-const int TrainID = 0;   // using this to show the id now as it is not in the usual train code i would have to start from 1 when i am taking the data form the level file
-const int TrainTICK = 1; // first value in the given data
+const int TrainID = 0;     // using this to show the id now as it is not in the usual train code i would have to start from 1 when i am taking the data form the level file
+const int TrainTICK = 1;   // first value in the given data
 const int T_x = 2;         // second value in the given data
 const int T_y = 3;         // thired value in the given data
 const int T_direction = 4; // fourth value in the given data
 const int T_colour = 5;    // fifth value in the given data
 
 // now taking destenation points to know where train needs to go so we can the use manhatan formula to evaluate the preority system
-const int T_des_X = 6; // sixth value int would be caluculated 
+const int T_des_X = 6; // sixth value int would be caluculated
 const int T_des_Y = 7;
 
-// TO check which trinas are active and which are not 
-
-const int T_status = 8;
+const int T_status = 8; // TO check which trinas are active and which are not
 
 // Taking an other constant for the total properties so that i can make an array of total train x total properties
 
-const int T_properties= 9;
-
-// array 
-extern int trains_data [Trains_max][T_properties];
-
-// For number of trains 
-extern int Number_Of_Trains;
+const int T_properties = 9;
 
 // ----------------------------------------------------------------------------
 // SWITCH CONSTANTS
@@ -55,35 +40,55 @@ extern int Number_Of_Trains;
 
 int const S_name = 0;
 int const S_Mode = 1;
-int const S_init = 2;
-int const S_K_up = 3;
-int const S_K_right = 3;
-int const S_K_down = 4;
-int const S_K_left = 5;
-int const S_state0 = 6;
-int const S_state1 = 7;
+const int S_State = 1; // instead of state1 and state0 using this so that both can be checked  0 = straight and turn while 1 = left / right
+const int S_VisualType = 2;
 
+int const S_K_up = 3;
+int const S_K_right = 4;
+int const S_K_down = 5;
+int const S_K_left = 6;
+
+// current state of the switches is being told by the variable below
+int const S_K_current_up = 7;
+int const S_K_current_right = 8;
+int const S_K_current_rdown = 9;
+int const S_K_current_rleft = 10;
+
+int const S_properties = 11;
+int const MAX_switches = 26;
 
 // ----------------------------------------------------------------------------
 // WEATHER CONSTANTS
 // ----------------------------------------------------------------------------
-int const weather;
+extern int weather;
 // ----------------------------------------------------------------------------
 // SIGNAL CONSTANTS
 // ----------------------------------------------------------------------------
-
+const int Signal_Green = 0;
+const int Signal_Yellow = 1;
+const int Signal_Red = 2;
 // ----------------------------------------------------------------------------
 // GLOBAL STATE: GRID
 // ----------------------------------------------------------------------------
+/* using extern int cause the row and column constants would be decalred in .cpp file and woudl be share here */
 
+extern int grid_rows;
+extern int grid_columns;
+
+extern char grid[MAX_rows][MAX_columns]; // THE MAXIMUM MAP
 // ----------------------------------------------------------------------------
 // GLOBAL STATE: TRAINS
 // ----------------------------------------------------------------------------
 
+// array
+extern int trains_data[Trains_max][T_properties];
+
+// For number of trains
+extern int Number_Of_Trains;
 // ----------------------------------------------------------------------------
 // GLOBAL STATE: SWITCHES (A-Z mapped to 0-25)
 // ----------------------------------------------------------------------------
-
+extern int switch_data[MAX_switches][S_properties];
 // ----------------------------------------------------------------------------
 // GLOBAL STATE: SPAWN POINTS
 // ----------------------------------------------------------------------------
@@ -95,15 +100,18 @@ int const weather;
 // ----------------------------------------------------------------------------
 // GLOBAL STATE: SIMULATION PARAMETERS
 // ----------------------------------------------------------------------------
-
+extern int tick;
+extern unsigned int seed;
 // ----------------------------------------------------------------------------
 // GLOBAL STATE: METRICS
 // ----------------------------------------------------------------------------
-
+extern int trains_crashed;
+extern int trains_arrived;
+extern int total_switch_flips;
 // ----------------------------------------------------------------------------
 // GLOBAL STATE: EMERGENCY HALT
 // ----------------------------------------------------------------------------
-
+extern int emergency_halt_timer;
 // ----------------------------------------------------------------------------
 // INITIALIZATION FUNCTION
 // ----------------------------------------------------------------------------
